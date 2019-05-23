a7e0aba471695459403407f1a1b371c3a8352923
antonoakmountain <antonoakmountain@gmail.com>
2019-05-23 12:35:17 +0200

    Update UI WIP

---
+++ TheMurderRoom/Assets/Scripts/Intro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Intro : MonoBehaviour {
+
+    private VideoPlayer videoPlayer;
+    private AsyncOperation loadScene;
+
+    void Start() {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        loadScene = SceneManager.LoadSceneAsync("PlayScene");
+        loadScene.allowSceneActivation = false;
+        StartCoroutine(AsyncLoadScene());
+
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+
+    void EndReached(VideoPlayer vp) {
+        vp.enabled = false;
+        // Allow scene load to finish
+        loadScene.allowSceneActivation = true;
+    }
+
+    IEnumerator AsyncLoadScene() {
+        while (!loadScene.isDone) {
+            yield return null;
+        }
+    }
+
+}
