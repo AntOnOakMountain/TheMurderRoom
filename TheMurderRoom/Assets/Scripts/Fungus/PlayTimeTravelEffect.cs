f4549bd01338da102f808a80e02126847c70f0c7
antonoakmountain <antonoakmountain@gmail.com>
2019-05-27 19:07:18 +0200

    Basic time travel light effect

---
+++ TheMurderRoom/Assets/Scripts/Fungus/PlayTimeTravelEffect.cs
@@ -0,0 +1,33 @@
+﻿// This code is part of the Fungus library (http://fungusgames.com) maintained by Chris Gregan (http://twitter.com/gofungus).
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
+using UnityEngine;
+
+namespace Fungus {
+    /// <summary>
+    /// Signals the game that the dialogue ends. 
+    /// </summary>
+    [CommandInfo("TheMurderRoom",
+                 "Play Time Travel Effect",
+                 "")]
+    [AddComponentMenu("")]
+    public class PlayTimeTravelEffect : Command {
+
+        [Tooltip("If the effect should play in reverse")]
+        [SerializeField] protected bool reverse;
+
+        protected virtual void CallTheMethod() {
+
+            TimeTravelLightEffect.instance.Activate(reverse);
+        }
+
+        public override void OnEnter() {
+            CallTheMethod();
+            Continue();
+        }
+
+        public override Color GetButtonColor() {
+            return new Color32(235, 191, 217, 255);
+        }
+    }
+}
---
+++ TheMurderRoom/Assets/Scripts/Graphics/TimeTravelLightEffect.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelLightEffect : MonoBehaviour {
+
+    public static TimeTravelLightEffect instance;
+
+    public float duration;
+    public float goalRange;
+
+    public float reverseDuration;
+
+    private float start;
+    private float end;
+
+    private Timer timer;
+    private new Light light;
+    private bool reverse;
+
+    public void Start() {
+        instance = this;
+        light = GetComponent<Light>();
+        gameObject.SetActive(false);
+    }
+
+    public void Activate(bool reverse) {
+        this.reverse = reverse;
+        if (reverse) {
+            start = goalRange;
+            end = 0;
+            timer = new Timer(reverseDuration);
+        }
+        else {
+            start = 0;
+            end = goalRange;
+            timer = new Timer(duration);
+        }
+        light.range = start;
+
+        gameObject.SetActive(true);
+        timer.Start();
+    }
+
+    public void Update() {
+        if (timer.IsActive()) {
+            if (reverse) {
+                light.range = Mathf.Lerp(start, end, Mathf.Sin(timer.TimePercentagePassed() * Mathf.PI * 0.5f));
+            }
+            else {
+                light.range = Mathf.Lerp(start, end, timer.TimePercentagePassed() * timer.TimePercentagePassed());
+            }
+        }
+        if (timer.IsDone()) {
+            timer.Pause();
+            gameObject.SetActive(false);
+        }
+    }
+}
