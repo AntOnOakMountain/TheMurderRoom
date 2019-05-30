630953b0eecd877b3c539a33e010bf95c737ae7f
antonoakmountain <antonoakmountain@gmail.com>
2019-05-30 14:42:12 +0200

    Added FPS counter for build version (show with f)

--- TheMurderRoom/Assets/Scripts/FPSDisplay.cs
+++ TheMurderRoom/Assets/Scripts/FPSDisplay.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class FPSDisplay : MonoBehaviour {
     float deltaTime = 0.0f;
 
+    bool active = false;
+
     void Update() {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Input.GetKeyDown("f")) {
+            active = !active;
+        }
     }
 
     void OnGUI() {
-        int w = Screen.width, h = Screen.height;
+        if (active) {
+            int w = Screen.width, h = Screen.height;
 
-        GUIStyle style = new GUIStyle();
+            GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 50);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 50;
-        style.normal.textColor = new Color(1f, 1f, 1f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
+            Rect rect = new Rect(0, 0, w, h * 2 / 50);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 50;
+            style.normal.textColor = new Color(1f, 1f, 1f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
     }
 }
